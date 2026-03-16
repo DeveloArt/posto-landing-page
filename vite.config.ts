9d261c27b6f6f3e1325f293bef9a4ff68ca47bde
Arkadiusz Daszkowski <daszkowskiarek145@gmail.com>
2026-03-16 23:52:48 +0100

    fix: update base path in Vite config for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: mode === "production" ? "/" : "/",
+    base: mode === "production" ? "/posto/" : "/",
     server: {
       port: 3000,
       host: "0.0.0.0",
